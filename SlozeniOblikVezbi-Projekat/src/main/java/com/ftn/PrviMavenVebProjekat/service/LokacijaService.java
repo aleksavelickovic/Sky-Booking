8e477d78459b92aed1d25e27a99072710bc6c16a
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2024-12-04 00:28:01 +0100

    Added LokacijaService interface

---
+++ SlozeniOblikVezbi-Projekat/src/main/java/com/ftn/PrviMavenVebProjekat/service/LokacijaService.java
@@ -0,0 +1,18 @@
+package com.ftn.PrviMavenVebProjekat.service;
+
+import java.util.List;
+
+import com.ftn.PrviMavenVebProjekat.model.Lokacija;
+
+public interface LokacijaService {
+
+	Lokacija findOne(Long id);
+
+	List<Lokacija> findAll();
+
+	Lokacija save(Lokacija lokacija);
+
+	Lokacija update(Lokacija lokacija);
+
+	Lokacija delete(Long id);
+}
