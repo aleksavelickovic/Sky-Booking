fbaa3d5dc741342be19ba75e99418e8c3696014f
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2024-12-23 18:12:04 +0100

    Added some Lokacija values to the database

--- SlozeniOblikVezbi-Projekat/src/main/db/CreateDatabase.sql
+++ SlozeniOblikVezbi-Projekat/src/main/db/CreateDatabase.sql
@@ -1,4 +1,25 @@
 DROP SCHEMA IF EXISTS webprojekat;
 CREATE SCHEMA webprojekat DEFAULT CHARACTER SET utf8;
-USE webprojekatt;
+USE webprojekat;
 
+CREATE TABLE lokacije (
+	id BIGINT AUTO_INCREMENT,
+    Grad VARCHAR(100) NOT NULL,
+    Drzava VARCHAR(100) NOT NULL,
+    Kontinent ENUM("Evropa", "Amerika", "Azija", "Australija", "Afrika", "Antartika", "Okeanija"),
+    primary key(id)
+);
+
+INSERT INTO lokacije (Grad, Drzava, Kontinent) VALUES
+('Beograd', 'Srbija', 'Evropa'),
+('New York', 'Sjedinjene Američke Države', 'Amerika'),
+('Tokio', 'Japan', 'Azija'),
+('Sidnej', 'Australija', 'Australija'),
+('Kairo', 'Egipat', 'Afrika'),
+('Buenos Aires', 'Argentina', 'Amerika'),
+('Pariz', 'Francuska', 'Evropa'),
+('Kejp Taun', 'Južnoafrička Republika', 'Afrika'),
+('Auckland', 'Novi Zeland', 'Okeanija'),
+('McMurdo Station', 'Antarktik', 'Antartika');
+
+SELECT * FROM lokacije;
