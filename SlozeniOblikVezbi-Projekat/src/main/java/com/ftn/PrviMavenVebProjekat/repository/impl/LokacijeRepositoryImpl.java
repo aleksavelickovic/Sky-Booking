89f4af74957dc4e06af4e403790eabbc2aefd077
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2024-12-23 17:57:58 +0100

    Added LokacijeRepositoryImpl class

---
+++ SlozeniOblikVezbi-Projekat/src/main/java/com/ftn/PrviMavenVebProjekat/repository/impl/LokacijeRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.ftn.PrviMavenVebProjekat.repository.impl;
+
+import java.util.List;
+
+import com.ftn.PrviMavenVebProjekat.model.Lokacija;
+import com.ftn.PrviMavenVebProjekat.repository.LokacijeRepository;
+
+public class LokacijeRepositoryImpl implements LokacijeRepository{
+
+	@Override
+	public Lokacija findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Lokacija> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int save(Lokacija lokacija) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Lokacija lokacija) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
