2022390d1be9645e5adbe05b6c783a618c40aaff
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-07-09 01:24:47 +0200

    Add admin page, new index template, and improve styles and navigation.
    - Added `admin.html` for admin-specific functionality.
    - Created new `index.html` with search and reservation features.
    - Enhanced `Uloga` enum with display names and `toString` method.
    - Updated hover effects in `Animation-Cards.css`.
    - Added new search bar styles in
    `Growing-Search-Bar-Animated-Text-Input.css`.
    - Deleted old static `index.html` file.
    - Updated navigation to include admin page link for admins.
    - Added `base` tag in `header-footer.html` for relative paths.
    - Enhanced `header-footer.html` with additional styles.
    - Improved button hover effects and navigation logic.

--- SlozeniOblikVezbi-Projekat/src/main/java/com/ftn/PrviMavenVebProjekat/model/Uloga.java
+++ SlozeniOblikVezbi-Projekat/src/main/java/com/ftn/PrviMavenVebProjekat/model/Uloga.java
@@ -1,5 +1,16 @@
 package com.ftn.PrviMavenVebProjekat.model;
 
 public enum Uloga {
-	PUTNIK, ADMIN
+	PUTNIK("PUTNIK"), ADMIN("ADMIN");
+
+	private final String displayName;
+
+	Uloga(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
 }
